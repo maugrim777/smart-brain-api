c35cd97ebc7ea22b3d5a45f3ab32e58372be3e04
maugrim777 <gabriel.vlasceanu@outlook.com>
2020-03-17 19:05:36 +0200

    connecting to db on heroku_ second attempt

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-shaped-64546',
-      user : 'maugrim777',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   }
 );
